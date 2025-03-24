4f8b5be8169545f3c8733dec470d98bcbc197ea2
Sebastian_Acevedo <acevedosebastian455@gmail.com>
2025-03-24 14:22:38 +0100

    XML y JSON

--- 2da_Eva/LenguajeMarcas/repasoXML_JSON/repaso2XML/script.js
+++ 2da_Eva/LenguajeMarcas/repasoXML_JSON/repaso2XML/script.js
@@ -18,7 +18,7 @@ cabecera.onclick = function () {
 }
 
 //Leer el archivo JSON local
-fetch('assets/configuracion_web.json')
+fetch('JSON2/configuracion_web.json')
     .then(function(response) {
         response.json()
         .then(function (data) {
@@ -33,7 +33,7 @@ fetch('assets/configuracion_web.json')
 
 //Función para mostrar la estructura completa del JSON
 function mostrarContenido() {
-    let contenidoJSON = datosConfig.configuracion_web;
+    let contenidoJSON = datosConfig[0];
 
     resultado.textContent = JSON.stringify(contenidoJSON, null, 2);
 
